e208b3721112528d286264a3d52ede42f8f20f0f
Ian-Tiggle <iantiggle14@gmail.com>
2026-09-01 13:41:23 -0400

    test

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,5 @@
+'''
+Python is a programming language that is for people who are new to coding.
+Python includes all of the basics of coding
+Using Python before you try any other programming language with set you up for success
+'''
---
+++ assignments/01_getting_started/01_02_hello_world_typos.py
@@ -0,0 +1,3 @@
+print("hello_world")
+# This is invalid syntax because of the captial letter in the function print
+# Print("hello_world")
