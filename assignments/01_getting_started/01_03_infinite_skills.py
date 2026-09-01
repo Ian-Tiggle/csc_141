6d44f5bfb7828a6d3e28bfffb80baa88a6cd907b
Ian-Tiggle <iantiggle14@gmail.com>
2026-09-01 13:50:54 -0400

    test2

--- assignments/01_getting_started/01_03_infinite_skills.py
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -2,6 +2,7 @@
 With my new skills I would like to create my own video game.
 I would also like to make my own app.
 The final thing that I want to create is an ai tutor that people cannot exploit.
+testing
 '''
 
 
