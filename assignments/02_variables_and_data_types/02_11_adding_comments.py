2c0e3e458c22b573f4c99b55db5717b275d82120
Ian-Tiggle <iantiggle14@gmail.com>
2026-09-08 13:25:56 -0400

    Assignment

--- assignments/02_variables_and_data_types/02_11_adding_comments.py
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -1,11 +1,1 @@
-# Ian Tiggle 9/7/2026
-filename = "python_notes.txt"
-print(filename.removesuffix(".txt"))
-
-
-person = "\tJames"
-
-print(person)
-print(person.lstrip())
-print(person.rstrip())
-print(person.strip())
+# Check  02_07 & 02_08
