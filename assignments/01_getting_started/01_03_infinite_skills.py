3dc5be34f83976dae8777884cd71242175423ad1
Ian-Tiggle <iantiggle14@gmail.com>
2026-09-10 13:21:58 -0400

    Fix

--- assignments/01_getting_started/01_03_infinite_skills.py
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -1,8 +1,11 @@
 '''
 With my new skills I would like to create my own video game.
 I would also like to make my own app.
-The final thing that I want to create is an ai tutor that people cannot exploit.
-testing
+The final thing that I want to create is an ai tool that will help people in any fields of work.
+I will build a team of diverse jobs that will help me create this ultimate ai tool that will help people get into all different types of work.
+Not just in computing fields.
+This will help people get a job that they are underqualified for by training them for it.
+
 '''
 
 
